c02a272ab09d69ff76381e332e73270c8d6bf82e
Cody Clark <codyjamesclark@gmail.com>
2024-07-23 09:28:30 -0500

    added profile view and edit button

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -1,3 +1,38 @@
-export const Profile = () => {
-    return <>PROFILE</>
+import { useEffect, useState } from "react"
+import { getUserById } from "../Services/UserService"
+import "./Profile.css"
+import { useNavigate } from "react-router-dom"
+
+export const Profile = ({currentUser}) => {
+
+    const navigate = useNavigate()
+
+    const [user, setUser] = useState({})
+
+    useEffect(() => {
+        if(currentUser) {
+            getUserById(currentUser.id).then(data => {
+                setUser(data)
+            })
+        }
+    }, [currentUser])
+
+    return (
+        <>
+            <div className="profile-container">
+                <div key={user.id} className="profile-info">
+                    <div>name: {user.name?.toLowerCase()}</div>
+                    <div>email: {user.email}</div>
+                    <div># of projects: {user.projects?.length}</div>
+                </div>
+            </div>
+            <div className="btn-container">
+                <div className="edit-btn" onClick={() => {
+                    navigate("/profile/edit")
+                }}>edit</div>
+            </div>
+            
+            
+        </>
+    )
 }
--- src/Services/UserService.jsx
+++ src/Services/UserService.jsx
@@ -2,3 +2,7 @@ export const getUserByEmail = (email) => {
     return fetch(`http://localhost:8088/users?email=${email}`).then(res => res.json())
 
 }
+
+export const getUserById = (userId) => {
+    return fetch(`http://localhost:8088/users/${userId}?_embed=projects`).then(res => res.json())
+}
